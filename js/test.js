fb4cd045df1d5bce70d3a01cb67113d1e9d3b77b
jlo <jlo@ua.pt>
2015-02-01 00:08:35 +0000

    js tests

---
+++ js/test.js
@@ -0,0 +1,20 @@
+/* Comentário */
+
+var resultado = soma(3,4); 
+
+document.write(resultado+"<br/>");
+
+for(var i=0;i<16;i++)
+	{ document.write("uma-string" - 2);
+		document.write("<br />");
+  }
+document.write("Batman");
+
+var a ="abc"; switch(a){
+case "abc": alert("string abc"); break; case 3: alert("inteiro 3"); break; default: alert("outro");
+}
+
+
+function soma(x,y) { 
+	return x+y;
+}
